1e3bfc51f793604d6864e3205784303346601ca4
xinherumen <3190494355@qq.com>
2024-02-20 14:43:04 +0800

    加入axios和elment-plus库

--- src/main.js
+++ src/main.js
@@ -2,5 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+// 设置axios
+app.config.globalProperties.$axios = axios
+
+// 使用Element Plus
+app.use(ElementPlus)
+
+// 挂载应用
+app.use(store).use(router).mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false
 })
+// // vue.config.js
+// module.exports = {
+//   chainWebpack: (config) => {
+//     config.plugin('define').tap((args) => {
+//       args[0]['__VUE_PROD_HYDRATION_MISMATCH_DETAILS__'] = JSON.stringify(true);
+//       return args;
+//     });
+//   },
+// };
